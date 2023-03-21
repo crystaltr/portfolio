c8e8c72741469299ea2f2d47e77d8424e0299467
crystaltr <ctru0002@student.monash.edu>
2023-03-21 20:12:58 +1100

    add about content

--- src/About.jsx
+++ src/About.jsx
@@ -6,11 +6,11 @@ class About extends Component {
         return (
             <>
             <div className= "container">
-            {/* <p><b>Hi there!</b> < br/>< br/>
+            <p><b>Hi there!</b> < br/>< br/>
                 I’m Crystal and I’m currently a UX designer at an Insurance company. I excel mainly in UX, however I have a growing interesting in UI and DesignOps. I’m currently working on growing my UI skills to become an all rounder designer to create meaningful actions for users. < br/>< br/>
                 Before discovering UX design I was initially going to pursue being a front-end developer! Luckily I found out UX is a lot more fun and I wouldn’t have to face anymore syntax and bugs...< br/>< br/>
                 My portfolio will probably be my last programming project so hopefully there isn’t too many bugs hiding!< br/>< br/>
-                Thank you for dropping by •ᴗ•</p>*/}</div> 
+                Thank you for dropping by •ᴗ•</p></div> 
             </>
            
         )
