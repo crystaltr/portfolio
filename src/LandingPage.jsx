d89c6f75ace847db92da4daaaea5f86b969b78e3
crystaltr <ctru0002@student.monash.edu>
2023-03-24 19:23:29 +1100

    update stylings

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './landingpage.css';
 import { Link } from 'react-router-dom';
-import chev from './chev.png';
 import retry from './retry.png';
 import payment from './payment.png';
 
@@ -10,25 +9,27 @@ class LandingPage extends Component {
     render() {
         return (
             <>
-            <div className='title'>
-              <h1>Intentional designs for < br/> everyday use</h1>
-            </div>
-            <div class="rect-container">
-                <div class="rect-item"></div>
-                <div class="rect-item"></div>
-                <div class="rect-item"></div>
-                <div class="rect-item"></div>
-                <div class="rect-item"></div>
-                <div class="rect-item"></div>
-                <div class="rect-item"></div>
-                <div class="rect-item"></div>
+            <div class='t-section'>
+                <div className='title'>
+                    <h1>Intentional designs for < br/> everyday use</h1>
+                </div>
+                <div class="rect-container">
+                    <div class="rect-item"></div>
+                    <div class="rect-item"></div>
+                    <div class="rect-item"></div>
+                    <div class="rect-item"></div>
+                    <div class="rect-item"></div>
+                    <div class="rect-item"></div>
+                    <div class="rect-item"></div>
+                    <div class="rect-item"></div>
+                </div>
             </div>
             <div className='p1-grid'>
                 <img src={payment} alt="payment" class='img1'/>
                 <div className='p1-description'>
                 <h1>Payment redesign</h1>
                 <p>A redesign of the payments pages to allow users to intuitively update <br/> and delete payment methods.</p>
-                <Link to="/project2" className="p1-btn"><b>View project</b> <img src={chev} width="25" height="25"/></Link>
+                <Link to="/project2" className="p1-btn">View project</Link>
                 </div>
             </div>
 
@@ -36,7 +37,7 @@ class LandingPage extends Component {
                 <div className='p2-description'>
                     <h1>Retry feature</h1>
                     <p>Giving users the ability to retry payments within the portal.</p>
-                    <Link to="/project1" className="p1-btn"><b>View project</b> <img src={chev} width="25" height="25"/></Link>
+                    <Link to="/project1" className="p1-btn">View project</Link>
                 </div>
                 <img src={retry} alt="retry" class='img2'/>
             </div>
